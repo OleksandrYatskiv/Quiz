92672082fdb97c99426e1d93be1d5da9360c2ef5
OleksandrYatskiv <yatsk1660@gmail.com>
2023-09-07 12:24:42 +0200

    Update Card.jsx

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,24 +4,24 @@ import React, { useState } from "react";
 import ModalWindow from "../ModalWindow/ModalWindow";
 
 export default function Card({ quiz }) {
-	const [isShowModal, setShowModal] = useState(false);
+  const [isShowModal, setShowModal] = useState(false);
 
-	const handleShowModal = () => {
-		setShowModal(!isShowModal);
-	};
+  const handleShowModal = () => {
+    setShowModal(!isShowModal);
+  };
 
-	return (
-		<>
-			<CardContainer>
-				<CardImg src={quiz.image}></CardImg>
-				<Typography sx={{ textAlign: 'center', margin: '10px 0' }} variant="h5">{quiz.name}</Typography>
-				<Typography sx={{ textAlign: 'center' }}>{quiz.description}</Typography>
-				<CardActionContainer>
-					<Action onClick={handleShowModal}>Show more info</Action>
-					<Action onClick={() => alert('Quiz started!')}>Start Quiz</Action>
-				</CardActionContainer>
-			</CardContainer>
-			{isShowModal && <ModalWindow quiz={quiz} />}
-		</>
-	);
+  return (
+    <>
+      <CardContainer>
+        <CardImg src={quiz.image}></CardImg>
+        <Typography sx={{ textAlign: 'center', margin: '10px 0' }} variant="h5">{quiz.name}</Typography>
+        <Typography sx={{ textAlign: 'center' }}>{quiz.description}</Typography>
+        <CardActionContainer>
+          <Action onClick={handleShowModal}>Show more info</Action>
+          <Action onClick={() => alert('Quiz started!')}>Start Quiz</Action>
+        </CardActionContainer>
+      </CardContainer>
+      {isShowModal && <ModalWindow quiz={quiz} />}
+    </>
+  );
 }
