82099dca469c9e110bd17dd2e6e59169f4ad5f3c
Sashko <yatsk1660@gmail.com>
2023-10-22 21:52:44 +0200

    made some changes

--- netlify.eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: 'your-base-config', // Extend your base ESLint config
-  rules: {
-    // Disable specific rules for Netlify builds
-    'react-hooks/exhaustive-deps': 'off',
-  },
-};
